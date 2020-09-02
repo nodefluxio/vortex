00de934ed92ddd72f130f93d888af688b6c569de
Alvin Prayuda <alvinprayuda@gmail.com>
2020-09-02 18:28:13 +0700

    Update src/development/vortex/development/core/engine/trainer/lr_scheduler/__init__.py
    Co-authored-by: Tri Wahyu Utomo <triwahyu34@gmail.com>

--- src/development/vortex/development/core/engine/trainer/lr_scheduler/__init__.py
+++ src/development/vortex/development/core/engine/trainer/lr_scheduler/__init__.py
@@ -4,16 +4,18 @@ from .tanh_lr_with_warmup import *
 from .step_lr_with_warmup import *
 
 step_update_map = {
-    'batch_update':['CyclicLR',
-                    'OneCycleLR',
-                    ],
-    'epoch_update':['StepLRWithWarmUp',
-                    'CosineLRWithWarmUp',
-                    'TanhLRWithWarmUp',
-                    'StepLR',
-                    'MultiStepLR',
-                    'ExponentialLR',
-                    'CosineAnnealingLR',
-                    'CosineAnnealingWarmRestarts'
-                    ]
+    'batch_update': [
+        'CyclicLR',
+        'OneCycleLR',
+    ],
+    'epoch_update': [
+        'StepLRWithWarmUp',
+        'CosineLRWithWarmUp',
+        'TanhLRWithWarmUp',
+        'StepLR',
+        'MultiStepLR',
+        'ExponentialLR',
+        'CosineAnnealingLR',
+        'CosineAnnealingWarmRestarts'
+    ]
 }
