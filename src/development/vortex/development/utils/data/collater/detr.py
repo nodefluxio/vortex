29ed9ceaf2bfda97084bdba4c76211e914c35431
triwahyuu <triwahyu34@gmail.com>
2020-09-14 12:47:44 +0700

    fix collater dataformat value none

--- src/development/vortex/development/utils/data/collater/detr.py
+++ src/development/vortex/development/utils/data/collater/detr.py
@@ -3,10 +3,10 @@ import torch
 from vortex.development.networks.models.detection.detr import NestedTensor
 from easydict import EasyDict
 
-class DETRColatte:
+class DETRCollate:
     def __init__(self, dataformat: dict):
         dataformat = {k: {n: torch.tensor(v, dtype=torch.long) if n == 'indices' else v for n,v in val.items()}
-            for k,val in dataformat.items()}
+            for k,val in dataformat.items() if val is not None}
         self.dataformat = EasyDict(dataformat)
         self.disable_image_auto_pad = True
 
@@ -19,7 +19,7 @@ class DETRColatte:
             out_target = {}
             if target.ndim != 2:
                 raise RuntimeError("expects dimensionality of target is 2 got %s" % target.ndim)
-            if self.dataformat.class_label is None:
+            if not "class_label" in self.dataformat or self.dataformat.class_label is None:
                 out_target['labels'] = torch.zeros(target.shape[0], dtype=torch.int64)
             else:
                 out_target['labels'] = torch.index_select(
@@ -37,10 +37,10 @@ class DETRColatte:
         return images, collated_targets
 
 
-supported_collater = ['DETRColatte']
+supported_collater = ['DETRCollate']
 
 def create_collater(collater: str, *args, **kwargs):
     if not collater in supported_collater:
         raise RuntimeError("unsupported collater %s, supported : %s" % (
             collater, ','.join(supported_collater)))
-    return DETRColatte(*args, **kwargs)
+    return DETRCollate(*args, **kwargs)
