f68145133e5c311146248617aa85d8920fbd6f85
triwahyuu <triwahyu34@gmail.com>
2021-01-05 10:18:35 +0700

    moving out

--- src/development/vortex/development/core/engine/deprecated/__init__.py
+++
@@ -1,3 +0,0 @@
-## forward create_trainer and create_validator to parent module
-from .validator import create_validator, register_validator, remove_validator, BaseValidator
-from .trainer import create_trainer, register_trainer, remove_trainer, BaseTrainer
