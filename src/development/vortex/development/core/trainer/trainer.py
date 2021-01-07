bafad03ef7f82b75eeb0c9df8e54244d9b6e3163
triwahyuu <triwahyu34@gmail.com>
2021-01-07 13:59:29 +0700

    add create logger

--- src/development/vortex/development/core/trainer/trainer.py
+++ src/development/vortex/development/core/trainer/trainer.py
@@ -1,9 +1,11 @@
 import pytorch_lightning as pl
+import pytorch_lightning.loggers as pl_loggers
+import pytorch_lightning.callbacks as pl_callbacks
 
 from pathlib import Path
 from copy import deepcopy
+from easydict import EasyDict
 
-from pytorch_lightning import callbacks as pl_callbacks
 from pytorch_lightning.trainer.connectors.checkpoint_connector import CheckpointConnector
 from pytorch_lightning.trainer.connectors.logger_connector import LoggerConnector
 from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
@@ -24,26 +26,34 @@ class TrainingPipeline:
     def __init__(self, config):
         super().__init__()
 
-        ## TODO: build model
-        self.model = None
-
         ## TODO: validate config
         self.config = config
 
         experiment_dir = str(Path('.').joinpath("experiments", config['experiment_name']))
         self.trainer = self.create_trainer(experiment_dir)
 
-        ## create dataset
-        self.datamodule = None
+        ## TODO: build model
+        self.model = self.create_model()
+
+        ## TODO: create dataset
+        self.datamodule = self.create_datamodule()
 
 
     def run(self):
         pass
 
-    def create_trainer(self, experiment_dir, config=None) -> pl.Trainer:
-        config = config if config else self.config
-        self.experiment_dir = experiment_dir
-        fname_prefix = config['experiment_name']
+    def create_model(self, config=None):
+        ## TODO: FINISH THIS!!
+        pass
+
+    def create_datamodule(self, config=None):
+        ## TODO: FINISH THIS!!
+        pass
+
+    def create_model_checkpoints(self, experiment_dir, config=None):
+        if config:
+            self.config = config
+        fname_prefix = self.config['experiment_name']
 
         ## patches for model checkpoint
         ModelCheckpoint.FILE_EXTENSION = ".pth"
@@ -62,7 +72,7 @@ class TrainingPipeline:
         if isinstance(available_metrics, list):
             available_metrics = {m: "min" if "loss" in m else "max" for m in available_metrics}
 
-        save_best_metrics = config['trainer']['save_best_metrics']
+        save_best_metrics = self.config['trainer']['save_best_metrics']
         if isinstance(save_best_metrics, str):
             save_best_metrics = [save_best_metrics]
 
@@ -76,8 +86,52 @@ class TrainingPipeline:
                 mode=available_metrics[m]
             ))
 
-        ## TODO: create loggers
-        loggers = True
+
+    def create_loggers(self, experiment_dir, config=None, no_log=False):
+        logger_map = {
+            'comet_ml': pl_loggers.CometLogger,
+            'ml_flow': pl_loggers.MLFlowLogger,
+            'neptune': pl_loggers.NeptuneLogger,
+            'tensorboard': pl_loggers.TensorBoardLogger,
+            'test_tube': pl_loggers.TestTubeLogger,
+            'wandb': pl_loggers.WandbLogger,
+            'csv_logger': pl_loggers.CSVLogger
+        }
+
+        if config:
+            self.config = config
+
+        logger_cfg = self.config["logging"]
+        if logger_cfg is None or no_log:
+            return False
+
+        if not isinstance(logger_cfg, (dict, EasyDict)):
+            raise RuntimeError("Unknown data type of 'config.logging' field, expected to have "
+                "'dict' type, got {}".format(type(logger_cfg)))
+        if "module" not in logger_cfg or "args" not in logger_cfg:
+            raise RuntimeError("logger config is incomplete, 'config.logging' is expected to have "
+                "'module' and 'args' attribute, got {}".format(list(logger_cfg)))
+
+        loggers = None
+        logger_module = None
+        if logger_cfg["module"] in logger_map:
+            logger_module = logger_map[logger_cfg["module"]]
+        if hasattr(logger_cfg["module"]):
+            logger_module = logger_cfg["module"]
+            loggers = logger_module(**logger_cfg["args"])
+        else:
+            raise RuntimeError("Unknown logger module name of '{}', available logger: {}"
+                .format(logger_cfg["module"], list(logger_map)))
+        return loggers
+
+    def create_trainer(self, experiment_dir, config=None) -> pl.Trainer:
+        if config:
+            self.config = config
+        self.experiment_dir = experiment_dir
+
+        callbacks = self.create_model_checkpoints(experiment_dir)
+
+        loggers = self.create_loggers(experiment_dir)
 
         ## patch for logger path (exclude 'lightning_logs' when logger not set)
         LoggerConnector.configure_logger = patch_configure_logger
@@ -86,7 +140,7 @@ class TrainingPipeline:
 
         trainer = pl.Trainer(
             gpus=1,
-            max_epochs=config['trainer']['epoch'],
+            max_epochs=self.config['trainer']['epoch'],
             logger=loggers,
             default_root_dir=self.experiment_dir,
             deterministic=True, 
@@ -94,6 +148,7 @@ class TrainingPipeline:
             weights_summary=None,
             move_metrics_to_cpu=True,
             callbacks=callbacks,
+            logger=loggers
         )
         ## patch for additional checkpoint data
         trainer.checkpoint_connector = CheckpointConnector(trainer)
