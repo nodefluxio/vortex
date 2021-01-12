ce75bfb167b82809d8acd4a840062e5dcafe8f4c
triwahyuu <triwahyu34@gmail.com>
2021-01-12 21:07:05 +0700

    add trainer test on save_best_metrics and save_epoch

--- tests/pipelines/test_trainer.py
+++ tests/pipelines/test_trainer.py
@@ -3,7 +3,6 @@ import torch
 import torch.nn as nn
 import pytorch_lightning as pl
 
-from pathlib import Path
 from copy import deepcopy
 from easydict import EasyDict
 from torch.utils.data import Dataset, DataLoader
@@ -87,17 +86,36 @@ class DummyModel(ModelBase):
         self.log('accuracy', acc, on_epoch=True)
 
 
-def patched_pl_trainer(experiment_dir, model):
+def patched_pl_trainer(experiment_dir, model, callbacks):
     TrainingPipeline._patch_trainer_components()
-    trainer = pl.Trainer(default_root_dir=experiment_dir)
+    trainer = pl.Trainer(
+        gpus=1 if torch.cuda.is_available() else None,
+        default_root_dir=experiment_dir,
+        callbacks=callbacks
+    )
     TrainingPipeline._patch_trainer_object(trainer)
 
+    ## setup accelerator
     trainer.accelerator_backend = trainer.accelerator_connector.select_accelerator()
     trainer.accelerator_backend.setup(model)
     trainer.accelerator_backend.train_loop = trainer.train
     trainer.accelerator_backend.validation_loop = trainer.run_evaluation
     trainer.accelerator_backend.test_loop = trainer.run_evaluation
+
+    ## dummy metrics data
+    metrics = {
+        'train_loss': torch.tensor(1.0891),
+        'accuracy': torch.tensor(0.7618)
+    }
+    trainer.logger_connector.callback_metrics = metrics
     return trainer
+
+def prepare_model(config, num_classes=5):
+    model = DummyModel(num_classes=num_classes)
+    model.config = config
+    model.class_names = ["label_"+str(n) for n in range(num_classes)]
+    return model
+
 
 ## TODO: test logger
 
@@ -140,24 +158,13 @@ def test_handle_validation_interval():
 
 
 ## TODO; test checkpoint
-def test_checkpoint_default_last(tmp_path: Path):
-    config = deepcopy(_REQUIRED_TRAINER_CFG)
-    config = EasyDict(config)
+def test_checkpoint_default_last(tmp_path):
+    config = EasyDict(deepcopy(_REQUIRED_TRAINER_CFG))
 
-    num_classes = 5
-    model = DummyModel(num_classes=num_classes)
-    model.config = config
-    model.class_names = ["label_"+str(n) for n in range(num_classes)]
-    trainer = patched_pl_trainer(str(tmp_path), model)
+    model = prepare_model(config)
+    ckpt_callbacks = TrainingPipeline.create_model_checkpoints(config, model)
+    trainer = patched_pl_trainer(str(tmp_path), model, ckpt_callbacks)
 
-    ## dummy metrics data
-    metrics = {
-        'train_loss': 1.0891,
-        'accuracy': 0.9618
-    }
-    trainer.logger_connector.callback_metrics = metrics
-
-    ckpt_callbacks = TrainingPipeline.create_model_checkpoints(str(tmp_path), config, model)
     ckpt_callbacks[0].on_pretrain_routine_start(trainer, model)
     ckpt_callbacks[0].on_validation_end(trainer, model)
 
@@ -168,18 +175,134 @@ def test_checkpoint_default_last(tmp_path: Path):
     ## check saved checkpoint
     checkpoint = torch.load(fpath)
     assert checkpoint['config'] == dict(config)
-    assert checkpoint['metrics'] == metrics
+    assert checkpoint['metrics'] == trainer.logger_connector.callback_metrics
     assert checkpoint['class_names'] == model.class_names
     assert checkpoint['vortex_version'] == vortex_version
     assert 'checkpoint_last' in checkpoint['callbacks']
 
 
-def test_checkpoint_save_best():
-    pass
+@pytest.mark.parametrize(
+    "save_best",
+    [
+        'accuracy',
+        'train_loss',
+        pytest.param(['accuracy'], id='accuracy on list'),
+        pytest.param(['accuracy', 'train_loss'], id='multiple checkpoint')
+    ]
+)
+def test_checkpoint_save_best(tmp_path, save_best):
+    config = deepcopy(_REQUIRED_TRAINER_CFG)
+    config['trainer'].update(dict(save_best_metrics=deepcopy(save_best)))
+    config = EasyDict(config)
+
+    model = prepare_model(config)
+    ckpt_callbacks = TrainingPipeline.create_model_checkpoints(config, model)
+    trainer = patched_pl_trainer(str(tmp_path), model, ckpt_callbacks)
+
+    if isinstance(save_best, str):
+        save_best = [save_best]
+    ## default save last
+    save_best.insert(0, None)
+    assert ckpt_callbacks[0].monitor == None
+    assert len(ckpt_callbacks) == len(save_best)+1
+
+    ## init all first
+    for callback in ckpt_callbacks:
+        callback.on_pretrain_routine_start(trainer, model)
+
+    for callback, monitor in zip(ckpt_callbacks, save_best):
+        assert callback.monitor == monitor
+        if monitor is None:
+            continue
+
+        delta = 0.2
+        opt_strategy = model.available_metrics[monitor]
+        original_metrics_val = deepcopy(trainer.logger_connector.callback_metrics)
+        prev_metrics_val = deepcopy(trainer.logger_connector.callback_metrics)
+
+        for action in (None, torch.add, torch.sub):
+            ## update metrics
+            if action is not None:
+                metrics = original_metrics_val
+                trainer.logger_connector.callback_metrics = {n: action(v, delta) for n, v in metrics.items()}
+
+            callback.on_validation_end(trainer, model)
+
+            fname = "{}-best_{}.pth".format(config['experiment_name'], monitor)
+            fpath = tmp_path.joinpath("version_0", "checkpoints", fname)
+            assert fpath.exists()
+
+            if (opt_strategy == 'max' and action == torch.add) or (opt_strategy == 'min' and action == torch.sub):
+                saved_metrics = trainer.logger_connector.callback_metrics
+            else:
+                saved_metrics = prev_metrics_val 
+
+            checkpoint = torch.load(fpath)
+            assert checkpoint['config'] == dict(config)
+            assert checkpoint['metrics'] == saved_metrics
+            assert checkpoint['class_names'] == model.class_names
+            assert checkpoint['vortex_version'] == vortex_version
+
+            ckpt_monitor_key = 'checkpoint_' + monitor
+            assert ckpt_monitor_key in checkpoint['callbacks']
+            assert checkpoint['callbacks'][ckpt_monitor_key]['monitor'] == monitor
+            assert checkpoint['callbacks'][ckpt_monitor_key]['best_model_score'] == saved_metrics[monitor]
+
+            ## step
+            trainer.current_epoch += 1
+            trainer.global_step += 2
+            prev_metrics_val = deepcopy(trainer.logger_connector.callback_metrics)
 
 
-def test_checkpoint_save_epoch():
-    pass
+@pytest.mark.parametrize(
+    "save_epoch", [1, 2]
+)
+def test_checkpoint_save_epoch(tmp_path, save_epoch):
+    config = deepcopy(_REQUIRED_TRAINER_CFG)
+    config['trainer'].update(dict(save_epoch=save_epoch))
+    config = EasyDict(config)
+
+    model = prepare_model(config)
+    ckpt_callbacks = TrainingPipeline.create_model_checkpoints(config, model)
+    trainer = patched_pl_trainer(str(tmp_path), model, ckpt_callbacks)
+
+    ## init all first
+    for callback in ckpt_callbacks:
+        callback.on_pretrain_routine_start(trainer, model)
+
+    for callback in ckpt_callbacks:
+        if not hasattr(callback, 'save_epoch'):
+            continue
+
+        assert callback.period == save_epoch
+
+        for _ in range(2):
+            epoch = trainer.current_epoch
+            callback.on_validation_end(trainer, model)
+
+            if (epoch+1) % save_epoch == 0:
+                fname = "{}-epoch={}.pth".format(config['experiment_name'], epoch)
+                fpath = tmp_path.joinpath("version_0", "checkpoints", fname)
+                assert fpath.exists()
+
+                checkpoint = torch.load(fpath)
+                assert checkpoint['config'] == dict(config)
+                assert checkpoint['metrics'] == trainer.logger_connector.callback_metrics
+                assert checkpoint['class_names'] == model.class_names
+                assert checkpoint['vortex_version'] == vortex_version
+
+                assert 'checkpoint_last' in checkpoint['callbacks']
+                assert 'checkpoint_epoch' in checkpoint['callbacks']
+                assert checkpoint['epoch'] == epoch+1
+
+            ## step
+            trainer.current_epoch += 1
+            trainer.global_step += 2
+
+    ## invalid value of 0
+    with pytest.raises(RuntimeError):
+        config['trainer'].update(dict(save_epoch=0))
+        ckpt_callbacks = TrainingPipeline.create_model_checkpoints(config, model)
 
 
 ## TODO: test metrics
