30ad20ca429479e994402c6703d9607de0c2cc9a
Fahri Ali Rahman <fahri@nodeflux.io>
2021-02-08 02:04:50 +0700

    expose model and graph_ops registry to top-level module

--- src/development/vortex/development/__init__.py
+++ src/development/vortex/development/__init__.py
@@ -1,2 +1,5 @@
 from .version import __version__
 from .utils import create_model
+# TODO: naming fix
+from .exporter.utils.onnx.graph_ops import graph_ops_registry as onnx_graph_ops
+from .networks.models import supported_models
