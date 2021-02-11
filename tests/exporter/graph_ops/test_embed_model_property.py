a19395568c2702274715c38da488dfb5e24fe9ea
Fahri Ali Rahman <fahri@nodeflux.io>
2021-02-12 00:00:03 +0700

    add simple metadata check

--- tests/exporter/graph_ops/test_embed_model_property.py
+++ tests/exporter/graph_ops/test_embed_model_property.py
@@ -4,6 +4,7 @@ from functools import partial
 from onnx import helper
 from onnx import AttributeProto, TensorProto, GraphProto
 from vortex.runtime.onnx.graph_ops.helper import get_metadata_prop
+from vortex.runtime.onnx.graph_ops.check_metadata import check_metadata
 
 from .dummy_model import dummy_model
 
@@ -43,3 +44,7 @@ def test_embed_model_property():
     assert other_prop is not None
     assert other_prop.key == 'other_prop'
     assert other_prop.value == '1234'
+
+    assert check_metadata(model)
+    model = dummy_model()
+    assert not check_metadata(model)
