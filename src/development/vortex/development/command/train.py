0c31f9c5895b3e670c93ffcfa6cf071ef85ef9cf
triwahyuu <triwahyu34@gmail.com>
2021-02-15 12:13:16 +0700

    change train cli to use new pl training pipeline

--- src/development/vortex/development/command/train.py
+++ src/development/vortex/development/command/train.py
@@ -4,8 +4,6 @@ import warnings
 from vortex.development.pipelines import TrainingPipeline
 from vortex.development.utils.parser import load_config
 
-description='Vortex training pipeline; will generate a Pytorch model file'
-
 
 def check_deprecated_args(args):
     if args.config is None and args.config_dep is not None:
@@ -23,35 +21,10 @@ def check_deprecated_args(args):
 
 def main(args):
     check_deprecated_args(args)
-    log_metric = not args.no_log
-
-    # Load configuration from experiment file
-    config = load_config(args.config)
-
-    ckpt_in_cfg = 'checkpoint' in config and config.checkpoint is not None
-    if ckpt_in_cfg:
-        config.checkpoint = config.checkpoint.rstrip(',')
-    if args.resume and (not ckpt_in_cfg or (ckpt_in_cfg and config.checkpoint == None)):
-        ## TODO: point to documentation for resume
-        raise RuntimeError("You choose to resume but 'checkpoint' is not specified. "
-            "Please specify 'checkpoint' option in your configuration file pointing "
-            "to model path used for resume. see documentation.")
-
-    # Override logger config in experiment file
-    if not log_metric:
-        config.logging = None
 
     # create training pipeline
-    train_executor = TrainingPipeline(config=config, config_path=args.config, 
-                                      hypopt=False, resume=args.resume)
-    output = train_executor.run()
-
-    epoch_losses = output.epoch_losses
-    val_metrics = output.val_metrics
-    learning_rates = output.learning_rates
-
-    print("\nloss: {}\nmetrics: {}\nlearning rates: {}".format(epoch_losses, 
-        val_metrics, learning_rates))
+    train_executor = TrainingPipeline(args.config, hypopt=False, resume=args.resume)
+    train_executor.run()
 
 
 def add_parser(subparsers, parent_parser):
