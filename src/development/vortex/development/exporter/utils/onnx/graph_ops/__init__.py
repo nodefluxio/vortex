aabbe212972e28ab39e442e322cc6ce6fcdb95b0
Fahri Ali Rahman <fahri@nodeflux.io>
2021-04-14 00:46:05 +0700

    cleanup export & graph ops init

--- src/development/vortex/development/exporter/utils/onnx/graph_ops/__init__.py
+++ src/development/vortex/development/exporter/utils/onnx/graph_ops/__init__.py
@@ -1,7 +1,5 @@
 from vortex.runtime.onnx.graph_ops.base_ops import GraphOpsBase
 from vortex.runtime.onnx.graph_ops.create_batch_output_sequence import CreateBatchOutputSequence
-from vortex.runtime.onnx.graph_ops.embed_class_names import EmbedClassNames
-from vortex.runtime.onnx.graph_ops.embed_output_format import EmbedOutputFormat
 from vortex.runtime.onnx.graph_ops.nms_iou_threshold_as_input import IOUThresholdAsInput
 from vortex.runtime.onnx.graph_ops.symbolic_shape_infer import SymbolicShapeInfer
 from vortex.runtime.onnx.graph_ops.embed_metadata import EmbedMetadata
@@ -14,8 +12,6 @@ from vortex.development.utils.registry import Registry
 GRAPH_OPS = Registry("graph_ops",base_class=GraphOpsBase)
 
 GRAPH_OPS.add(CreateBatchOutputSequence)
-GRAPH_OPS.add(EmbedClassNames)
-GRAPH_OPS.add(EmbedOutputFormat)
 GRAPH_OPS.add(IOUThresholdAsInput)
 GRAPH_OPS.add(SymbolicShapeInfer)
 GRAPH_OPS.add(EmbedMetadata)
