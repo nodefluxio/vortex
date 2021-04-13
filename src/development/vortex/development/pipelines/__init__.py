0ff094ac92d4843f78b5d420ef3d39b110c8f55c
Fahri Ali Rahman <fahri@nodeflux.io>
2021-04-13 21:11:14 +0700

    remove unused modules

--- src/development/vortex/development/pipelines/__init__.py
+++
@@ -1,5 +0,0 @@
-from .trainer import TrainingPipeline
-from .validation_pipeline import PytorchValidationPipeline,IRValidationPipeline
-from .export_pipeline import GraphExportPipeline
-from .prediction_pipeline import PytorchPredictionPipeline, IRPredictionPipeline
-from .hypopt_pipeline import HypOptPipeline
