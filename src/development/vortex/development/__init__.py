545f8e17b717be48462edfb0a7c8bc113dd640d8
Fahri Ali Rahman <fahri@nodeflux.io>
2021-04-14 00:53:56 +0700

    cleanup init(s)

--- src/development/vortex/development/__init__.py
+++ src/development/vortex/development/__init__.py
@@ -1,5 +1,4 @@
 from .version import __version__
-from .utils import create_model
 
 # expose registry to top-level for shorthand
 from .exporter.utils.onnx.graph_ops import GRAPH_OPS as ONNX_GRAPH_OPS
